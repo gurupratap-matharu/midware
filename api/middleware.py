9da0fbeb105649fa631110becccad249ea57f447
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-07 15:42:27 -0300

    saving request model right from the middleware

--- api/middleware.py
+++ api/middleware.py
@@ -1,4 +1,7 @@
 import logging
+import time
+
+from api.models import Request
 
 logger = logging.getLogger(__name__)
 
@@ -8,9 +11,26 @@ class PaymentProcessorMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        _t = time.time()
         response = self.get_response(request)
+        _t = int((time.time() - _t) * 1000)
 
-        logger.debug(request)
-        logger.debug(response)
+        logger.info(request)
+        logger.info(response)
+
+        try:
+            Request(
+                user=request.user,
+                endpoint=request.get_full_path(),
+                response_code=response.status_code,
+                method=request.method,
+                remote_address=self.get_client_ip(request),
+                exec_time=_t,
+                body_response=str(response.content),
+                body_request=str(request.body)
+            ).save()
+
+        except (ValueError, AttributeError):
+            pass
 
         return response
