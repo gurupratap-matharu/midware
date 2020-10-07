04f7b8e2aa2497c9a11b51df8a786706d00ff889
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-07 14:30:43 -0300

    initial request model with body attribute

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Request(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    body = models.CharField(max_length=5000, blank=True)
